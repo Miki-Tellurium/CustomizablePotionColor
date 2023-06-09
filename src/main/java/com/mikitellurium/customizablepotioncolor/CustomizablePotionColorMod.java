e459223edefb37a3e41dfd117c6ecc5ded54b692
Miki_Tellurium <michelegamer14@gmail.com>
2023-06-09 15:16:43 +0200

    Moved config registration to client side init

--- src/main/java/com/mikitellurium/customizablepotioncolor/CustomizablePotionColorMod.java
+++ src/main/java/com/mikitellurium/customizablepotioncolor/CustomizablePotionColorMod.java
@@ -1,14 +1,8 @@
 package com.mikitellurium.customizablepotioncolor;
 
-import com.mikitellurium.customizablepotioncolor.config.VanillaPotionConfig;
 import net.fabricmc.api.ModInitializer;
-
-import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientLifecycleEvents;
-import net.fabricmc.loader.api.FabricLoader;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.io.IOException;
 
 public class CustomizablePotionColorMod implements ModInitializer {
 
@@ -17,15 +11,6 @@ public class CustomizablePotionColorMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-
-		ClientLifecycleEvents.CLIENT_STARTED.register((client) -> {
-			try {
-				VanillaPotionConfig.initConfig();
-				LOGGER.info("Loaded vanilla potions colors config file");
-			} catch (IOException e) {
-				throw new RuntimeException(e);
-			}
-		});
 
 	}
 
