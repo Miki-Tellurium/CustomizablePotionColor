d92aa49bd31820e77651be5861a1d705efcd5eb9
Miki_Tellurium <michelegamer14@gmail.com>
2023-06-11 19:27:19 +0200

    Updated config library

--- src/main/java/com/mikitellurium/customizablepotioncolor/api/TelluriumConfig.java
+++ src/main/java/com/mikitellurium/customizablepotioncolor/api/TelluriumConfig.java
@@ -7,9 +7,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-/* Tellurium Config
- * Forge Version 1.2.0
- *
+/*
  * You can use and modify this freely if you want, just respect the License
  * and credit me.
  *
@@ -36,85 +34,150 @@ import java.util.Scanner;
  * SOFTWARE.
  */
 
+/**
+ * A class used to create and load simple config files.
+ * @author Miki Tellurium
+ * @version 1.3.0-Forge
+ */
 @SuppressWarnings("rawtypes")
 public class TelluriumConfig {
 
     private static final String fileExtension = ".properties";
 
-    // Config file builder
+    /**
+     * The Builder is an object used to define new config entries,
+     * as well as saving and loading config files.
+     */
     public static class Builder {
 
-        private final String file;
-        private final String fileName; // Using the mod id suggested
+        private final String file; // The path of the config file
 
         private final List<String> COMMENTS = new ArrayList<>();
         private final List<ConfigEntry> ENTRIES = new ArrayList<>();
 
+        /**
+         * Builder object constructor.
+         * @param fileName the name of the config file
+         */
         public Builder(String fileName) {
-            this.fileName = fileName;
-            this.file = FMLPaths.CONFIGDIR.get().resolve(this.fileName + fileExtension).toString();
+            // Using the mod id suggested
+            this.file = FMLPaths.CONFIGDIR.get().resolve(fileName + fileExtension).toString();
         }
 
+        /**
+         * Return the path of the config file
+         *
+         * @return the config file path
+         */
         public String getConfigFilePath() {
             return file;
         }
 
+
+        /**
+         * Add a comment to the config file.
+         * <p>
+         * Comments will be written at the top of the file before
+         * any entry.
+         *
+         * @param comment the comment to write to the config file
+         * @return the builder object
+         */
         public Builder comment(String comment) {
             COMMENTS.add(comment);
             return this;
         }
 
-        // Boolean
+        /**
+         * Makes an entry that holds a boolean value.
+         *
+         * @param key the name of the entry
+         * @param defaultValue the default value of the entry
+         * @return the config entry that was created
+         */
         public ConfigEntry<Boolean> define(String key, boolean defaultValue) {
             ConfigEntry<Boolean> newEntry = new ConfigEntry<>(this, key, defaultValue);
             ENTRIES.add(newEntry);
             return newEntry;
         }
 
-        // Integer
+        /**
+         * Makes an entry that holds a integer value.
+         *
+         * @param key the name of the entry
+         * @param defaultValue the default value of the entry
+         * @return the config entry that was created
+         */
         public ConfigEntry<Integer> define(String key, int defaultValue) {
             ConfigEntry<Integer> newEntry = new ConfigEntry<>(this, key, defaultValue);
             ENTRIES.add(newEntry);
             return newEntry;
         }
 
+        /**
+         * Makes an entry that holds a integer value.
+         * <p>
+         * This value will always stay between the specified
+         * range (inclusive).
+         *
+         * @param key the name of the entry
+         * @param minValue the minimum value this entry can have
+         * @param maxValue the maximum value this entry can have
+         * @param defaultValue the default value of the entry
+         * @return the config entry that was created
+         */
         public IntRangeConfigEntry defineInRange(String key, int minValue, int maxValue, int defaultValue) {
             IntRangeConfigEntry newEntry = new IntRangeConfigEntry(this, key, minValue, maxValue, defaultValue);
             ENTRIES.add(newEntry);
             return newEntry;
         }
-        
-        // Double
+
+        /**
+         * Makes an entry that holds a double value.
+         *
+         * @param key the name of the entry
+         * @param defaultValue the default value of the entry
+         * @return the config entry that was created
+         */
         public ConfigEntry<Double> define(String key, double defaultValue) {
             ConfigEntry<Double> newEntry = new ConfigEntry<>(this, key, defaultValue);
             ENTRIES.add(newEntry);
             return newEntry;
         }
 
-        // Long
+        /**
+         * Makes an entry that holds a long value.
+         *
+         * @param key the name of the entry
+         * @param defaultValue the default value of the entry
+         * @return the config entry that was created
+         */
         public ConfigEntry<Long> define(String key, long defaultValue) {
             ConfigEntry<Long> newEntry = new ConfigEntry<>(this, key, defaultValue);
             ENTRIES.add(newEntry);
             return newEntry;
         }
 
-        // String
+        /**
+         * Makes an entry that holds a string value.
+         *
+         * @param key the name of the entry
+         * @param defaultValue the default value of the entry
+         * @return the config entry that was created
+         */
         public ConfigEntry<String> define(String key, String defaultValue) {
             ConfigEntry<String> newEntry = new ConfigEntry<>(this, key, defaultValue);
             ENTRIES.add(newEntry);
             return newEntry;
         }
 
-        private ConfigEntry<?> getConfigEntry(String key) {
-            for (ConfigEntry entry : ENTRIES) {
-                if (entry.getKey().equals(key)) {
-                    return entry;
-                }
-            }
-
-            return null;
-        }
-
+        /**
+         * Build a new config file.
+         * <p>
+         * If the file already exist also load all its entries values.
+         * Call this during the initialization phase of the game.
+         * @throws IOException if the file is not found
+         */
         public void build() throws IOException {
             File file = new File(this.file);
             if (file.exists()) {
@@ -123,6 +186,14 @@ public class TelluriumConfig {
             save();
         }
 
+        /**
+         * Saves the current loaded values to the config file.
+         * <p>
+         * This is automatically called from the {@link #build()} method
+         * but can also be called individually to save values when they
+         * are changed during the execution of the game.
+         * @throws IOException if it wasn't possible to write to the config file
+         */
         public void save() throws IOException{
             FileWriter writer = new FileWriter(file);
             final String newline = System.lineSeparator(); // Wrap text
@@ -133,6 +204,7 @@ public class TelluriumConfig {
                     writer.write("#" + s + newline);
                 }
             }
+
             writer.write(newline);
             writer.write("[Settings]" + newline);
             writer.write(newline);
@@ -161,6 +233,9 @@ public class TelluriumConfig {
             writer.close();
         }
 
+        /*
+         * Loads values from the config file
+         */
         private void load() throws IOException {
             File file = new File(this.file);
             Scanner reader = new Scanner(file);
@@ -169,6 +244,9 @@ public class TelluriumConfig {
             }
         }
 
+        /*
+         * Reads an entry from the config file and load its value
+         */
         @SuppressWarnings("unchecked")
         private void parseConfigEntry(String string, int line) {
             if (isValueLine(string)) {
@@ -197,18 +275,36 @@ public class TelluriumConfig {
                         }
 
                     } else {
-                        throw new IllegalConfigValueException("Syntax error in config file \"" + this.getConfigFilePath() + "\" on line " + line);
+                        throw new IllegalConfigValueException(
+                                "Syntax error in config file \"" + this.getConfigFilePath() + "\" on line " + line);
                     }
 
                 } else {
-                    throw new IllegalConfigValueException("Syntax error in config file \"" + this.getConfigFilePath() + "\" on line " + line);
+                    throw new IllegalConfigValueException(
+                            "Syntax error in config file \"" + this.getConfigFilePath() + "\" on line " + line);
                 }
 
             }
 
         }
 
-        // Check if the current line we are reading is an entry or a comment
+        /*
+         * Returns the config entry corresponding to the specified key
+         * or null if the config entry wasn't found
+         */
+        private ConfigEntry<?> getConfigEntry(String key) {
+            for (ConfigEntry entry : ENTRIES) {
+                if (entry.getKey().equals(key)) {
+                    return entry;
+                }
+            }
+
+            return null;
+        }
+
+        /*
+         * Check if the current line we are reading is an entry or a comment
+         */
         private boolean isValueLine(String line) {
             if (line.isEmpty()) return false;
             else if (line.startsWith("#") || line.startsWith("[")) return false;
@@ -217,7 +313,10 @@ public class TelluriumConfig {
 
     }
 
-    // Object that saves values
+    /**
+     * An object used to save a config value in a
+     * config file.
+     */
     @SuppressWarnings("fieldCanBeLocal")
     public static class ConfigEntry<T> {
 
@@ -233,18 +332,30 @@ public class TelluriumConfig {
             this.defaultValue = defaultValue;
         }
 
+        /**
+         * @return the builder that holds this entry
+         */
         public Builder getBuilder() {
             return builder;
         }
 
+        /**
+         * @return the key of this entry
+         */
         public String getKey() {
             return key;
         }
 
+        /**
+         * @return the default value of this entry
+         */
         public T getDefaultValue() {
             return defaultValue;
         }
 
+        /**
+         * @return the current loaded value for this entry
+         */
         public T getValue() {
             if (value == null || value.toString().isBlank()) {
                 return defaultValue;
@@ -253,22 +364,41 @@ public class TelluriumConfig {
             return value;
         }
 
+        /**
+         * Change the currently loaded value of this entry.
+         * <p>
+         * If this is called during the execution of the game, use
+         * {@link Builder#save()} before the game close to save the
+         * new value to the config file.
+         * @param value the new value
+         */
         public void setValue(T value) {
             this.value = value;
         }
 
-        // The comment get placed before the actual field
+        /**
+         * Set the comment for this entry.
+         * @param comment the comment to write before the entry
+         * @return the config entry that was commented
+         */
         public ConfigEntry<T> comment(String comment) {
             this.comment = comment;
             return this;
         }
 
+        /*
+         * Get the comment for this entry
+         */
         private String getComment() {
             return comment;
         }
 
     }
 
+    /**
+     * An object used to save a config value that has to be
+     * in a certain range.
+     */
     public static class IntRangeConfigEntry extends ConfigEntry<Integer> {
 
         private final int minValue;
@@ -280,14 +410,31 @@ public class TelluriumConfig {
             this.maxValue = maxValue;
         }
 
+        /**
+         * @return the minimum value this entry can have
+         */
         public int getMinValue() {
             return minValue;
         }
 
+        /**
+         * @return the maximum value this entry can have
+         */
         public int getMaxValue() {
             return maxValue;
         }
 
+        /**
+         * Change the currently loaded value of this entry.
+         * <p>
+         * If the new value is out of the specified range for this entry
+         * it's automatically set to the closest value inside the range.
+         * <p>
+         * If this is called during the execution of the game, use
+         * {@link Builder#save()} before the game close to save the
+         * new value to the config file.
+         * @param value the new value
+         */
         @Override
         public void setValue(Integer value) {
             if (value < minValue) {
@@ -299,6 +446,11 @@ public class TelluriumConfig {
             }
         }
 
+        /**
+         * Set the comment for this entry.
+         * @param comment the comment to write before the entry
+         * @return the config entry that was commented
+         */
         @Override
         public IntRangeConfigEntry comment(String comment) {
             super.comment(comment);
@@ -307,6 +459,10 @@ public class TelluriumConfig {
 
     }
 
+    /**
+     * An exception used to handle illegal values specified
+     * in the config file.
+     */
     public static class IllegalConfigValueException extends IllegalArgumentException {
 
         public IllegalConfigValueException() {
